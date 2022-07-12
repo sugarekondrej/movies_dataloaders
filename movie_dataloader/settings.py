ad482366b9de01bd7e7cfe90d58f42bbdef8ec03
sugarekondrej <ondrej.sugarek@gmail.com>
2022-07-12 20:44:11 +0100

    Implemented POSTGRESQL DB and PSYCOPG2

--- movie_dataloader/settings.py
+++ movie_dataloader/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'movie_dataloader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movies_dataloader',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
